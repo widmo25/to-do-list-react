b84bf85111a99c9e086035184e8ae8e4f0bbb995
Michał Wodniczak <michal.wodniczak@o2.pl>
2021-06-07 16:46:01 +0200

    repair Tasks Component

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -3,8 +3,18 @@ import "./style.css"
 const Tasks = (props) => (
     <ul className="list">
         {props.tasks.map(task => (
-            <li key={task.id}>
-                {task.content}
+            <li key={task.id}
+                className={`list__item ${props.done && props.hideDoneTasks ? "list__item--hidden" : ""}`}
+            >
+                <button className="list__button">
+                    {task.done ? "✔" : ""}
+                </button>
+                <p className={`list__task\ ${task.done ? "list__task--done" : ""}`}>
+                    {task.content}
+                </p>
+                <button className="list__button list__button--higligthed">
+                    🗑
+                </button>
             </li>
         ))}
     </ul>

193ab5cc14a84b1891e8e3dff4e089c39e9069f3
Michał Wodniczak <michal.wodniczak@o2.pl>
2021-06-11 12:50:11 +0200

    destructuring

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,15 +1,15 @@
 import "./style.css"
 
-const Tasks = ({tasks,done,hideDone}) => (
+const Tasks = ({ tasks, done, hideDone,}) => (
     <ul className="list">
         {tasks.map(task => (
             <li key={task.id}
-                className={`list__item ${done && hideDone ? "list__item--hidden" : ""}`}
+                className={`list__item ${task.done && hideDone ? "list__item--hidden" : ""}`}
             >
                 <button className="list__button">
                     {task.done ? "✔" : ""}
                 </button>
-                <p className={`list__task ${task.done ? "list__task--done" : ""}`}>
+                <p className={`list__task ${done ? "list__task--done" : ""}`}>
                     {task.content}
                 </p>
                 <button className="list__button list__button--higligthed">
